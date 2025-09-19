96c5143313c290ea680e3ab12a8728196ebbd0d5
Sharanya <vsharanya111@gmail.com>
2025-09-19 11:53:47 +0530

    xyz

---
+++ test.c++
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    //int x,y,s;
+    cout<<"hello";
+}
---
+++ test.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+    <h1>test</h1>
+     <?php 
+     echo "Hello world";
+     ?>
+    </body>
+</html>
